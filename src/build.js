a4cc125398383f7a8e79a940ad51b1c1d698defa
smdmitry <smdmitry@gmail.com>
2019-05-13 14:57:44 +0300

    init

---
+++ src/build.js
@@ -0,0 +1,21 @@
+let coins = require('index.js');
+
+coins.getCoins();
+
+module.exports = {
+  coins: coins,
+  getCoins: getCoins,
+};
+
+/*
+XTZ: delegating: {
+      name: 'Baking',
+    },
+Eth like:
+    gas: {
+      ticker: 'Gwei',
+      title: 'Gas',
+    },
+  Other:
+    KMDVOTE2019
+ */
